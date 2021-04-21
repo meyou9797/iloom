14d2d40a0beb7a68a21b662948b341ed958922c6
meyou9797 <80095780+meyou9797@users.noreply.github.com>
2021-04-21 23:15:46 +0900

    일룸2차업로드

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//슬릭구현
+$(document).ready(function(){
+    $('.mainSlider').slick({
+       prevArrow:'<div class="prevBtn">이전</div>',
+       nextArrow:'<div class="nextBtn">다음</div>',
+        dots:true,
+       });
+})
